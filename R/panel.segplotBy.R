6d75af38034f3e0670fa46ac9a204721f3cfd7f3
Walmes Zeviani <walmeszeviani@gmail.com>
2015-07-25 15:38:08 -0300

    arguments documented together.

--- R/panel.segplotBy.R
+++ R/panel.segplotBy.R
@@ -1,16 +1,11 @@
 #' @title Lattice panel to non overlapping segments in \code{segplot()}
+#' 
 #' @name panel.segplotBy
 #'
 #' @description This panel allows no overlapping of segments in
 #' \code{latticeExtra::segplot()}.
 #'
-#' @param x see \code{\link[latticeExtra]{segplot}}.
-#' 
-#' @param y see \code{\link[latticeExtra]{segplot}}.
-#' 
-#' @param centers see \code{\link[latticeExtra]{segplot}}.
-#' 
-#' @param subscripts see \code{\link[latticeExtra]{segplot}}.
+#' @param x,y,centers,subscripts,... see \code{\link[latticeExtra]{segplot}}.
 #' 
 #' @param groups the grouping variable. Must be a factor.
 #' 
@@ -21,13 +16,12 @@
 #' @seealso \code{\link[latticeExtra]{segplot}}
 #'
 #' @export
+#' 
 #' @examples
-#' \donttest{
 #' require(doBy)
 #' require(multcomp)
 #' require(plyr)
 #' require(latticeExtra)
-#' ## source("apc.R"); source("apmc.R")
 #' 
 #' m0 <- lm(breaks~wool*tension, data=warpbreaks)
 #' X <- LSmatrix(m0, effect=c("wool", "tension"))
@@ -47,11 +41,9 @@
 #' 
 #' segplot(tension~lwr+upr, centers=estimate, data=L, draw=FALSE,
 #'         panel=panel.segplotBy, groups=L$wool, f=0.05)
-#' }
 panel.segplotBy <- function(x, y, z, centers, subscripts, groups, f, ...){
     d <- 2*((as.numeric(groups)-1)/(nlevels(groups)-1))-1
     z <- as.numeric(z)+f*d
     panel.segplot(x, y, z, centers=centers,
                   subscripts=subscripts, ...)
 }
-
