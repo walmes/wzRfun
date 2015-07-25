8d893c4dc0bd1c0b7c7a0ff3960de7cc75181e96
Walmes Zeviani <walmeszeviani@gmail.com>
2015-07-25 15:40:44 -0300

    documentation of subscripts and remove ... from function.

--- R/prepanel.cbH.R
+++ R/prepanel.cbH.R
@@ -1,4 +1,5 @@
 #' @title Lattice panels for error bars and envelop bands
+#' 
 #' @name prepanel.cbH
 #'
 #' @description Used to do or add error bars or envelop bands in 
@@ -10,12 +11,14 @@
 #'
 #' @param uy upper limit.
 #'
+#' @param subscripts see \code{\link[lattice]{panel.xyplot}}
+#'
 #' @return None is returned.
 #'
 #' @seealso \code{\link{panel.cbH}}
 #'
 #' @export
-prepanel.cbH <- function(y, ly, uy, subscripts, ...){
+prepanel.cbH <- function(y, ly, uy, subscripts){
     ly <- as.numeric(ly[subscripts])
     uy <- as.numeric(uy[subscripts])
     y <- as.numeric(y[subscripts])
