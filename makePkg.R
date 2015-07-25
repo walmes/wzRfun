2ad8d4694472e3f3423b14bd742ac56725d4154f
Walmes Zeviani <walmeszeviani@gmail.com>
2015-07-25 16:15:05 -0300

    check() and build() added.

--- makePkg.R
+++ makePkg.R
@@ -1,9 +1,20 @@
 require(roxygen2)
 
-getwd()
-setwd("../")
+## getwd()
+## setwd("../")
+setwd("/home/walmes/GitHub/")
 
 roxygenise(package.dir="wzRfun")
 
 ## system("R CMD check wzRfun")
 ## system("R CMD build wzRfun")
+
+require(devtools)
+
+setwd("/home/walmes/GitHub/wzRfun/")
+file.remove(".#makePkg.R")
+check()
+build()
+
+## require(wzRfun)
+## packageVersion("wzRfun")
