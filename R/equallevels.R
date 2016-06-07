76850667df0346e66e975deffadbb8a6b681e730
Walmes Zeviani <walmes@ufpr.br>
2016-06-07 15:31:48 -0300

    Improve documentation of equallevels.

--- R/equallevels.R
+++ R/equallevels.R
@@ -1,37 +1,35 @@
-#' @title Make that to data frames have the same factor levels
 #' @name equallevels
-#'
-#' @description If two data frames have factor ou character columns with
-#' the same name, those in the first will have the same level order as
-#' those in the second. So, in terms of factor columns, these data
-#' frames will have the same levels in the same order. Character columns
-#' in the first will be converted to factor.
-#' 
-#' @param x the target data frame.
-#'
-#' @param y the reference data frame.
-#'
-#' @return the restructred data frame.
-#'
 #' @export
+#' @author Walmes Zeviani, \email{walmes@ufpr.br}.
+#' @title Make Two Data Frames Have The Same Factor Levels
+#' @description If two data frames have factors ou characters columns
+#'     with the same column names, those in the first will have the same
+#'     level order as those in the second. So, for factors, these data
+#'     frames will have the same levels in the same order. Characters in
+#'     the first will be converted to factor.
+#' @param x the target data frame.
+#' @param y the reference data frame.
+#' @return the restructured data frame.
 #' @examples
-#' \donttest{
-#' a <- data.frame(Species=as.character(
-#'                     sample(iris$Species, 10, repl=TRUE)),
-#'                 stringsAsFactors=FALSE)
+#'
+#' a <- data.frame(Species = as.character(
+#'                     sample(iris$Species, size = 20, repl = TRUE)),
+#'                 stringsAsFactors = FALSE)
 #' str(a)
+#'
 #' b <- equallevels(a, iris)
 #' str(b)
-#' }
-equallevels <- function(x, y){
-    if(is.data.frame(x) & is.data.frame(y)){
-        com <- intersect(names(x), names(y))
-        for(i in com){
-            if(!is.null(levels(y[,i]))){
-                x[,i] <- factor(x[,i], levels=levels(y[,i]))
+#'
+equallevels <- function(x, y) {
+    if (is.data.frame(x) & is.data.frame(y)) {
+        com <- intersect(x = names(x), y = names(y))
+        for (i in com) {
+            if (!is.null(levels(y[, i]))) {
+                x[, i] <- factor(x[, i], levels = levels(y[, i]))
             }
         }
-        x
+        return(x)
+    } else {
+        stop("`x` and `y` must be a data.frame.")
     }
-    else stop("`x` and `y` must be a data.frame.")
 }
