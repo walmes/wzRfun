857937a4ae89718805d42c2664335234563878f2
Walmes Zeviani <walmes@ufpr.br>
2016-06-20 20:35:34 -0300

    Add welcome message when loading wzRfun.

---
+++ R/zzz_onAttach.R
@@ -0,0 +1,18 @@
+.onAttach <- function (lib, pkg) {
+    pkg.info <- drop(read.dcf(
+        file = system.file("DESCRIPTION", package = "wzRfun"),
+        fields = c("Package", "Title", "Version", "Date", "URL")
+    ))
+    dashes <- paste0(rep("----------", times = 7), collapse = "")
+    packageStartupMessage(
+        paste0(dashes, "\n  ",
+               pkg.info["Package"], ": ", pkg.info["Title"], "\n\n  ",
+               "For collaboration, support or bug report, visit:\n    ",
+               sub(x = pkg.info["URL"],
+                   pattern = "\n",
+                   replacement = "\n    "), "\n\n  ",
+               pkg.info["Package"], " version ", pkg.info["Version"],
+               " (build in ", pkg.info["Date"], ") was loaded.\n",
+               dashes)
+    )
+}
