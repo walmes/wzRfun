ca64b07d69e45e4df834f93dc27be2f08bc1a46c
Walmes Zeviani <walmes@ufpr.br>
2019-07-10 20:59:37 -0300

    Adds options to suppress start up message.

--- R/zzz_onAttach.R
+++ R/zzz_onAttach.R
@@ -1,18 +1,26 @@
 .onAttach <- function (lib, pkg) {
-    pkg.info <- drop(read.dcf(
-        file = system.file("DESCRIPTION", package = "wzRfun"),
-        fields = c("Package", "Title", "Version", "Date", "URL")
-    ))
-    dashes <- paste0(rep("----------", times = 7), collapse = "")
-    packageStartupMessage(
-        paste0(dashes, "\n  ",
-               pkg.info["Package"], ": ", pkg.info["Title"], "\n\n  ",
-               "For collaboration, support or bug report, visit:\n    ",
-               sub(x = pkg.info["URL"],
-                   pattern = "\n",
-                   replacement = "\n    "), "\n\n  ",
-               pkg.info["Package"], " version ", pkg.info["Version"],
-               " (build in ", pkg.info["Date"], ") was loaded.\n",
-               dashes)
-    )
+    startup <- getOption("wzRfun_startup_message", TRUE)
+    if (startup) {
+        pkg.info <- drop(read.dcf(
+            file = system.file("DESCRIPTION", package = "wzRfun"),
+            fields = c("Package", "Title", "Version", "Date", "URL")
+        ))
+        dashes <- paste0(rep("----------", times = 7), collapse = "")
+        packageStartupMessage(
+            paste0(
+                dashes, "\n  ",
+                pkg.info["Package"], ": ",
+                pkg.info["Title"], "\n\n  ",
+                "For collaboration, support or bug report, visit:\n    ",
+                sub(x = pkg.info["URL"],
+                    pattern = "\n",
+                    replacement = "\n    "), "\n\n  ",
+                pkg.info["Package"],
+                " version ",
+                pkg.info["Version"],
+                " (build in ", pkg.info["Date"],
+                ") was loaded.\n",
+                dashes)
+        )
+    }
 }
