87b953c47cdd096e4438b2f92de3b72aa04fda9d
Walmes Zeviani <walmes@ufpr.br>
2019-07-11 12:28:08 -0300

    Adds shell script to deploy package.

---
+++ _deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Cheks the package: devtools::build(...)."
+echo "Creates the *.check/ directory."
+Rscript -e "library(devtools); load_all(); check(manual = TRUE, vignettes = FALSE, check_dir = '.')"
+
+echo "Builds the package: devtools::build(...)."
+echo "Creates the *.tar.gz file."
+Rscript -e "library(devtools); load_all(); build(path = '.', manual = TRUE, vignettes = FALSE)"
+
+echo "Creates the *.zip file."
+TARGZ=$(ls wzRfun*.tar.gz | tail -n 1)
+ZIP=$(basename "$TARGZ" .tar.gz)
+rm -v "$ZIP.zip"
+cd wzRfun.Rcheck/
+zip -r "../$ZIP.zip" wzRfun
+cd ../
+
+echo "Compressed files available."
+ls wzRfun_*
+
+# Upload.
+echo "Uploading files to server."
+rsync -avzp \
+      wzRfun*.tar.gz \
+      wzRfun*.zip \
+      --progress \
+      --rsh="ssh -p$PATAXOP" "$PATAXO:~/public_html/pacotes/"
+
+exit 0
